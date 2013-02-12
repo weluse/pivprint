fc8c4ef9563049efeaa0c0fe6a3050cafc14ed76
Pascal Hartig <phartig@rdrei.net>
2013-02-12 13:44:29 +0100

    Set media type for cssNode

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,15 +1,16 @@
 (function bookmarklet() {
 
-    // Insert CSS node
+    // Insert CSS node.
     var cssNode = document.createElement('link'),
         ts = (new Date()).getTime(),
         styles;
     cssNode.href = 'https://raw.github.com/weluse/pivprint/master/css/print.css?' + ts;
     cssNode.setAttribute('rel', 'stylesheet');
+    cssNode.setAttribute('media', 'screen, print');
 
     document.body.appendChild(cssNode);
 
-    // Make all stylesheets print aware
+    // Make all stylesheets print aware.
     styles = document.querySelectorAll('link[media=screen]');
     Array.prototype.slice.call(styles).forEach(function (el) {
         el.setAttribute('media', 'screen, print');
